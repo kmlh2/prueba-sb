9239b6ba31333062ad3ef2e36975e8f4726bcf60
kmlh2 <camilo.hernandez.q@gmail.com>
2022-11-12 20:29:52 -0300

    ajustes aplication

--- src/main/java/com/store/prueba/PruebaApplication.java
+++ src/main/java/com/store/prueba/PruebaApplication.java
@@ -1,7 +1,6 @@
 package com.store.prueba;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -27,7 +26,6 @@ public class PruebaApplication {
 				PruebaApplication.class)) {
 			context.getBean(HttpServer.class).bindNow().onDispose().block();
 		}
-//		SpringApplication.run(PruebaApplication.class, args);
 	}
 
 	@Profile("default")
